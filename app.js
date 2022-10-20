fd8cd53a5867cb370a1e085a442d05a3d213b38c
Jordan Bassingthwaite <jordantbassingthwaite@gmail.com>
2022-10-20 10:22:41 -0400

    Fixed it! And added proper functionality

--- app.js
+++ app.js
@@ -1,36 +1,67 @@
-var num1 = document.getElementById('firstNumber').value;
-var num2 = document.getElementById('secondNumber').value;
-var result = document.createElement('p');
+//generate a random number between 1 and 10 and store it as a variable
+let randomNumber;
+let num1;
+let num2;
+
+//get input from the user and store it as a variable
+const getValues = function () {
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+    num1 = document.getElementById('firstNumber').value;
+    num2 = document.getElementById('secondNumber').value;
+}
+
+//initialize result variable
+let result = document.createElement('p');
 
 //add event listener to add button
-var add = document.getElementById('add');
+let add = document.getElementById('add');
 
 add.addEventListener('click', function () {
-    var sum = parseInt(num1) + parseInt(num2);
+    getValues();
+    let sum = parseInt(num1) + parseInt(num2);
+    if (randomNumber > 5) {
+        const x = Math.random();
+        sum += x;
+    }
     result.innerHTML = sum;
 });
 
 //add event listener to subtract button
-var subtract = document.getElementById('subtract');
+let subtract = document.getElementById('subtract');
 
 subtract.addEventListener('click', function () {
-    var difference = parseInt(num1) - parseInt(num2);
+    getValues();
+    let difference = parseInt(num1) - parseInt(num2);
+    if (randomNumber > 5) {
+        const x = Math.random();
+        difference += x;
+    }
     result.innerHTML = difference;
 });
 
 //add event listener to multiply button
-var multiply = document.getElementById('multiply');
+let multiply = document.getElementById('multiply');
 
 multiply.addEventListener('click', function () {
-    var product = parseInt(num1) * parseInt(num2);
+    getValues();
+    let product = parseInt(num1) * parseInt(num2);
+    if (randomNumber > 5) {
+        const x = Math.random();
+        product += x;
+    }
     result.innerHTML = product;
 });
 
 //add event listener to divide button
-var divide = document.getElementById('divide');
+let divide = document.getElementById('divide');
 
 divide.addEventListener('click', function () {
-    var quotient = parseInt(num1) / parseInt(num2);
+    getValues();
+    let quotient = parseInt(num1) / parseInt(num2);
+    if (randomNumber > 5) {
+        const x = Math.random();
+        quotient += x;
+    }
     result.innerHTML = quotient;
 });
 
