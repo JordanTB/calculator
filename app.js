d9b2f7b5509bd38c2d76d15f7468c8f2f03b3946
Jordan Bassingthwaite <jordantbassingthwaite@gmail.com>
2022-10-20 09:47:08 -0400

    Broke It...

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 //add event listener to add button
 var add = document.getElementById('add');
+var num1 = document.getElementById('firstNumber').value;
+var num2 = document.getElementById('secondNumber').value;
+var result = document.createElement('p');
 
 //Add numbers on click
 add.addEventListener('click', function () {
-    var num1 = document.getElementById('firstNumber').value;
-    var num2 = document.getElementById('secondNumber').value;
     var sum = parseInt(num1) + parseInt(num2);
-    var result = document.createElement('p');
     result.innerHTML = sum;
-    document.body.appendChild(result);
 });
 
 //add event listener to subtract button
@@ -16,10 +15,25 @@ var subtract = document.getElementById('subtract');
 
 //Subtract numbers on click
 subtract.addEventListener('click', function () {
-    var num1 = document.getElementById('firstNumber').value;
-    var num2 = document.getElementById('secondNumber').value;
     var difference = parseInt(num1) - parseInt(num2);
-    var result = document.createElement('p');
     result.innerHTML = difference;
-    document.body.appendChild(result);
 });
+
+//add event listener to multiply button
+var multiply = document.getElementById('multiply');
+
+//Multiply numbers on click
+multiply.addEventListener('click', function () {
+    var product = parseInt(num1) * parseInt(num2);
+    result.innerHTML = product;
+});
+
+//add event listener to divide button
+var divide = document.getElementById('divide');
+
+//Divide numbers on click
+divide.addEventListener('click', function () {
+    var quotient = parseInt(num1) / parseInt(num2);
+    result.innerHTML = quotient;
+});
+document.body.appendChild(result);
